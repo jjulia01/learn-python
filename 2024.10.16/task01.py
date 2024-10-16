f95cd80f63829e38c93f14125e4f71404d16672a
jjulia01 <153107471+jjulia01@users.noreply.github.com>
2024-10-16 19:36:14 +0300

    task01 решена

--- 2024.10.16/task01.py
+++ 2024.10.16/task01.py
@@ -1,3 +1,12 @@
 """Задача 2024.10.16.01
 
 Написать функцию для генерации строки из n строчных латинских букв"""
+
+import random
+def generate_random_string(n):
+    random_letters='abcdefghijklmnopqrstuvwxyz'
+    return ''.join(random.choice(random_letters)for _ in range(n))
+if __name__=='__main__':
+    n = 10
+    result = generate_random_string(n)
+    print(result)
